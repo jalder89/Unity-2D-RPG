68030ecc0670adb5fcbdfa263e60c0dacaf10a7a
Jessica Alder <jessalder1989@gmail.com>
2025-06-22 21:38:13 -0700

    Implemented hurt flash and timer

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private float hurtFlashDuration = 0.1f;
+
+    public float currentTimeInGame;
+    public float lastTimeWasDamaged;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        HurtFlashWhenHit();
+    }
+
+    private void HurtFlashWhenHit()
+    {
+        currentTimeInGame = Time.time;
+
+        if (currentTimeInGame > lastTimeWasDamaged + hurtFlashDuration)
+        {
+            if (spriteRenderer.color != Color.red)
+            {
+                TurnRed();
+            }
+        }
+    }
+
     public void TakeDamage()
     {
+        spriteRenderer.color = Color.yellow;
+        lastTimeWasDamaged = Time.time;
         Debug.Log(gameObject.name + " took some damage");
+    }
+
+    private void TurnRed()
+    {
+        spriteRenderer.color = Color.red;
     }
 }
