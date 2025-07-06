f4116f2e9d4d7a1acedd71c8b02f1b79b50fec16
Jessica Alder <jessalder1989@gmail.com>
2025-07-06 00:53:05 -0700

    Added parallax background scripts and art.

---
+++ Assets/Scripts/Parallax/ParallaxBackground.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float lastCameraPositionX;
+    private float cameraHalfWidth;
+
+    [SerializeField] private ParallaxLayer[] backgroundLayers;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        CalculateBackgroundWidth();
+    }
+
+    private void FixedUpdate()
+    {
+        float currentCameraPositionX = mainCamera.transform.position.x;
+        float distanceToMove = currentCameraPositionX - lastCameraPositionX;
+        lastCameraPositionX = currentCameraPositionX;
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
+        UpdateLayerPosition(distanceToMove, cameraLeftEdge, cameraRightEdge);
+    }
+
+    private void UpdateLayerPosition(float distanceToMove, float cameraLeftEdge, float cameraRightEdge)
+    {
+        foreach (ParallaxLayer parallaxLayer in backgroundLayers)
+        {
+            parallaxLayer.Move(distanceToMove);
+            parallaxLayer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+
+    private void CalculateBackgroundWidth()
+    {
+        foreach (ParallaxLayer parallaxLayer in backgroundLayers)
+        {
+            parallaxLayer.CalculateImageWidth();
+        }
+    }
+}
